b1fb8975b15b6044eb3d3eeef6a6a32d20b536e8
ZhongDerer <zhongderer@gmail.com>
2019-02-07 16:57:26 +0800

    debug on ipad

--- Dashboard/DrawingBoard.js
+++ Dashboard/DrawingBoard.js
@@ -468,7 +468,12 @@ var Drawing = RichBase.extend({
                     this.set("clicking", false);
                 }
             ],
-            "click": [],
+            "click": [
+                function (event) {
+                    event = EventUtil.getEvent(event);
+                    alert(event.target.id);
+                }
+            ]
         },
         "adjustCanvas": {"click": [
             function(event){
@@ -3879,7 +3884,6 @@ var Stretch = RichBase.extend({
                 function(event){
                     event = EventUtil.getEvent(event);
                     event.preventDefault();
-                    alert("body touchmove");
                     if (this.canvasWrap.style.zIndex !== -1 && this._ctrlEvent.flag === true) {   //使虚线框在最前)
                         this.canvasWrap.style.zIndex = -1;
                     }
