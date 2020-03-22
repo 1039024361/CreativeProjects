103170a22e2a0e85dd14a8b3e53d65bab189582e
zhongderer <1039024361@qq.com>
2020-03-22 19:22:38 +0800

    add PWA

--- Dashboard/main.js
+++ Dashboard/main.js
@@ -8,7 +8,7 @@ if (!window.addEventListener || !window.history || !window.requestAnimationFrame
 // enable service worker
 if ('serviceWorker' in navigator) {
         // register service worker
-        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('/CreativeProjects/Dashboard/service-worker.js').then(function (registration) {
             // 注册成功
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }).catch(function (err) {                   
